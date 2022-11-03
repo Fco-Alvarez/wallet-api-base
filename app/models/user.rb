0036c74ce9012477732a560abb2c0989e4b2cb09
your_github_fco-alvarez <your_github_fco.alvarez@icloud.com>
2022-11-03 14:51:25 -0300

    creacion de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+  has_secure_password
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Invalid email" }
   validates :rol, inclusion: { in: %w(admin regular),
                                message: "%{value} is not a valid rol" }
