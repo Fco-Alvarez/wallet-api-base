575a5c6352a66b960c6b41513c77f2815c13ffe5
Miguel Rivas <mrivaslezama@gmail.com>
2022-11-04 21:54:03 -0300

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :transactions, only: [:create, :show]
+  resources :transactions, only: [:create, :show, :update]
   resources :users, only: [:create]
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
