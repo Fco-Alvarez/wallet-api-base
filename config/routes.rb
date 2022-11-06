268c9258781aae574347f8cd3b0bee79b76209a5
hilario06 <hilario.huaman.6@gmail.com>
2022-11-06 17:19:05 -0500

    updated test routes index, show and create for transactions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         resources :users
         post '/auth/login', to: 'authentication#login'
         post '/auth/register',  to: 'authentication#register'
-        resources :transactions, only: %i[index create]
+        resources :transactions, only: %i[index create show]
       end
     end
   end
