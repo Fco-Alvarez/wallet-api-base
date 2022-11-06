feb88f34579d59ab96e84a584c731753b4b4fdb8
hilario06 <hilario.huaman.6@gmail.com>
2022-11-06 16:59:57 -0500

    added test routes the login and register user

---
+++ spec/routing/login_and_register_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Routes the login and registration' do
+  it 'User registration' do
+    expect(post: '/api/v1/auth/register').to route_to(
+      format: 'json',
+      controller: 'api/v1/authentication',
+      action: 'register'
+    )
+  end
+
+  it 'User login' do
+    expect(post: '/api/v1/auth/login').to route_to(
+      format: 'json',
+      controller: 'api/v1/authentication',
+      action: 'login'
+    )
+  end
+end
