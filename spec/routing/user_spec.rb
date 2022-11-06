6073ed8e9490d827f739938d6ef0b258ac558769
hilario06 <hilario.huaman.6@gmail.com>
2022-11-06 17:02:59 -0500

    added test users routes for index and show

---
+++ spec/routing/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Routes of the users' do
+  it 'Route index' do
+    expect(get: '/api/v1/users').to route_to(
+      format: 'json',
+      controller: 'api/v1/users',
+      action: 'index'
+    )
+  end
+
+  it 'Route show' do
+    expect(get: '/api/v1/users/1').to route_to(
+      format: 'json',
+      controller: 'api/v1/users',
+      action: 'show',
+      id: '1'
+    )
+  end
+end
