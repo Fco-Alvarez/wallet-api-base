bc3c5610b53efe252535ae7601f127b4313c3ce4
hilario06 <hilario.huaman.6@gmail.com>
2022-11-10 15:25:39 -0500

    added test model Account

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -19,5 +19,15 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations of the user model' do
+    subject { build(:account) }
+
+    it 'Validate if there is a relationship with user' do
+      should belong_to(:user)
+    end
+
+    it 'Validate relationship with movements' do
+      should have_many(:movements)
+    end
+  end
 end
